acb7ba3ed02dd2212394a0a77a31f046f473f10e
felipe-barroso45 <felipeb.oliveira0405@gmail.com>
2025-03-28 15:33:55 -0300

    Aula_5

--- Aula 4/RPG/RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
+++ Aula 4/RPG/RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235bb10880702717650590bc350bc31f131675fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e60423b06f6e12c54083e5ba486882e05796f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
